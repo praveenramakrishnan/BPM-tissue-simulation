ddc904aec8fdebc5a2a4a7ce1d6f6bea0e3aac5b
Praveen <pravin.nitc@gmail.com>
2025-02-14 11:22:18 +0000

    removed incomplete example

--- examples/mie_scattering/comparison_mie_and_bpm_different_media/utils_bpm/read_efield_input_data.m
+++
@@ -1,7 +0,0 @@
-function [efield_initial, x_grid, y_grid] = read_efield_input_data(filename_efield_input)
-    data_efield_input = load(filename_efield_input);
-    efield_initial = data_efield_input.efield_illumination{1}(:,:,1);
-    x_grid = data_efield_input.x_grid;
-    y_grid = data_efield_input.y_grid;
-end
-
