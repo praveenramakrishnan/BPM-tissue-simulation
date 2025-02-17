fc4c539261b2a30e8d4471a579d3a4d316e25fa0
Praveen <pravin.nitc@gmail.com>
2025-02-17 17:41:26 +0000

    moved background refractive index calculation outside run_beam_propagation function

--- examples/bpm_tissue_model_tomocube_data/input_parameters_tomocube_data.m
+++ examples/bpm_tissue_model_tomocube_data/input_parameters_tomocube_data.m
@@ -1,4 +1,7 @@
 filename_tomocube_data_directory = '~/POL_NAS/tomocube_data/';
 addpath(filename_tomocube_data_directory);
 filename_tomocube_data = append(filename_tomocube_data_directory, 'S014_converted_HT3D_0_reshaped');
+% If not known a priori, the background refractive index may be computed as the average of
+% the refractive index data
+refractive_index_background = 1.3333; 
 save(append(mfilename, '.mat'));
