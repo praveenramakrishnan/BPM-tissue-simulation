786d601279a7c7d20da1c093fb077beeac1b7def
Praveen <pravin.nitc@gmail.com>
2025-02-22 22:03:37 +0000

    removed unused function

--- src/utils_bpm_solution/strip_vectors.m
+++
@@ -1,4 +0,0 @@
-function [vector_stripped] = strip_vectors(input_vector, Nx, Ny)
-    [Nx_full, Ny_full] = size(input_vector);
-    vector_stripped = input_vector(floor((Nx_full-Nx)/2)+1:floor((Nx_full+Nx)/2), floor((Ny_full-Ny)/2)+1:floor((Ny_full+Ny)/2), :);
-end
